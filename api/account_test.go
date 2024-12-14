d1034956f72167edd37306b6310ca631e0f5ba06
imran4u <er.imran4u@gmail.com>
2024-12-14 11:39:44 +0530

    Respose body match

--- api/account_test.go
+++ api/account_test.go
@@ -1,11 +1,13 @@
 package api
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
-	"time"
 
 	mockdb "github.com/imran4u/simple-bank/db/mock"
 	db "github.com/imran4u/simple-bank/db/sqlc"
@@ -13,6 +15,27 @@ import (
 	"github.com/stretchr/testify/require"
 	"go.uber.org/mock/gomock"
 )
+
+func randomAccount() db.Account {
+	return db.Account{
+		ID:       util.RandomInt(1, 1000),
+		Owner:    util.RandomName(),
+		Currency: util.RandomCurrency(),
+		Balance:  util.RandomAmount(),
+		// CreatedAt: time.Now(),   //ignore time case other it will create problem in equal.
+	}
+}
+
+func requiredBodyMachAccount(t *testing.T, body *bytes.Buffer, account db.Account) {
+	data, err := io.ReadAll(body)
+	require.NoError(t, err)
+
+	var gotAccount db.Account
+	err = json.Unmarshal(data, &gotAccount)
+	require.NoError(t, err)
+	require.Equal(t, gotAccount, account)
+
+}
 
 func TestGetAccount(t *testing.T) {
 	accout := randomAccount()
@@ -37,15 +60,24 @@ func TestGetAccount(t *testing.T) {
 
 	server.router.ServeHTTP(recorder, req)
 	require.Equal(t, http.StatusOK, recorder.Code)
+	requiredBodyMachAccount(t, recorder.Body, accout)
 
 }
 
-func randomAccount() db.Account {
-	return db.Account{
-		ID:        util.RandomInt(1, 1000),
-		Owner:     util.RandomName(),
-		Currency:  util.RandomCurrency(),
-		Balance:   util.RandomAmount(),
-		CreatedAt: time.Now(),
-	}
-}
+// Table drivern test set to cover all possible senario
+
+// func TestGetAccountAppPossiblies(t *testing.T){
+// 	account := randomAccount()
+
+// 	//Ananomous struct
+// 	testCase := []struct{
+// 		name string,
+// 		accountId int64,
+// 		buildStub func(store *mockdb.MockStore),
+// 		// checkResponse func(t *testing.T, )
+
+// 	}{
+
+// 	}
+
+// }
