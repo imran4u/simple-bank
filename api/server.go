af5e79d0276239ada1743554f437dc259434e351
imran4u <er.imran4u@gmail.com>
2024-12-14 17:50:17 +0530

    transfer apis with currency validation

--- api/server.go
+++ api/server.go
@@ -22,6 +22,8 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts", server.listAccount)
 	router.DELETE("/account/:id", server.deleteAccount)
 
+	router.POST("/transfer", server.createTransfer)
+
 	server.router = router
 	return &server
 }
